c30eff604b1f891458c113e90c3fb8ea67fb79ef
egorTriceps <92254080+egorTriceps@users.noreply.github.com>
2025-08-14 23:38:14 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ echo "Installing dependencies..."
 sudo apt-get install -y nmap traceroute figlet lolcat cowsay toilet nmcli iwlist iwconfig
 
 # Give execute permission to the main script
-chmod +x network_scan.sh
+chmod +x netscan.sh
 
 # Run the script automatically
 echo "Starting NetScan Pro..."
-sudo ./network_scan.sh
+sudo ./netscan.sh
